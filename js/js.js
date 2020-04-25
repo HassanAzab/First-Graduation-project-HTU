2e7dff5780011add6b7044fd72045423ebf57e55
HassanAzab <35637741+HassanAzab@users.noreply.github.com>
2020-04-25 21:19:55 +0300

    Resturant
    bootsrap website

--- js/js.js
+++ js/js.js
@@ -1,2 +1,27 @@
+/*.programming-section,.Graphic-section,markiting-section*/
+$(document).ready(function(){
+    $(".p1").click(function(){
+   $("#soup").show();
+        $("#burger").hide();
+         $("#pizza").hide();
+        
+    });
+});
+$(document).ready(function(){
+    $(".p2").click(function(){
+           $("#burger").show();
+            $("#soup").hide();
+         $("#pizza").hide();
+        
+    });
+});
+$(document).ready(function(){
+    $(".p3").click(function(){
+         $("#pizza").show();
+        $("#soup").hide();
+        $("#burger").hide();
+        
+    });
+});
 
- 
+
